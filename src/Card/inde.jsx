5547dc7a39c8a26a9cd70b2d590dc2d5027592f1
vic-147 <dmkorps@gmail.com>
2023-05-08 12:41:41 -0400

    :lipstick: grid y rellenado de productos

--- src/Card/inde.jsx
+++ src/Card/inde.jsx
@@ -1,22 +1,22 @@
-const Card = () => {
+const Card = (data) => {
   return (
     <div className="bg-white cursor-pointer w-56 h-60 rounded-lg">
       <figure className="relative mb-2 w-full h-4/5">
         <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">
-          Electronics
+          {data.data.category.name}
         </span>
         <img
           className="w-full h-full object-cover rounded-lg"
-          src="https://images.pexels.com/photos/16525020/pexels-photo-16525020.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          alt="cel"
+          src={data.data.images[0]}
+          alt={data.data.title}
         />
         <div className="absolute bottom-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1">
           +
         </div>
       </figure>
       <p className="flex justify-between">
-        <span className="text-lg font-light">cel</span>
-        <span className="text-lg font-medium">$10</span>
+        <span className="text-lg font-light ml-2">{data.data.title}</span>
+        <span className="text-lg font-medium mr-2">${data.data.price}</span>
       </p>
     </div>
   );
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,11 +1,26 @@
+import { useState, useEffect } from "react";
 import { Layout } from "../../Components/Layout";
 import { Card } from "../../Card/inde";
 
+const apiUrl = import.meta.env.VITE_API;
+
 export default function Home() {
+  const [items, setItems] = useState(null);
+
+  useEffect(() => {
+    fetch(`${apiUrl}/products`)
+      .then((response) => response.json())
+      .then((data) => setItems(data));
+  }, []);
+
   return (
-      <Layout>
-        Home
-        <Card />
-      </Layout>
+    <Layout>
+      Home
+      <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+        {items?.map((item) => (
+          <Card data={item} key={item.id} />
+        ))}
+      </div>
+    </Layout>
   );
 }
